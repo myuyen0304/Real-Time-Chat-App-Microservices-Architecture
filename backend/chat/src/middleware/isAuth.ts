e2eb91fdc2412cf1b8bf0f835ef56e628dc621bf
myuyen0304 <134291922+myuyen0304@users.noreply.github.com>
2025-12-02 12:50:05 +0700

    Change name of Secret variable

--- backend/chat/src/middleware/isAuth.ts
+++ backend/chat/src/middleware/isAuth.ts
@@ -1,45 +1,51 @@
 import type { NextFunction, Request, Response } from "express";
-import jwt, { type JwtPayload } from 'jsonwebtoken';
+import jwt, { type JwtPayload } from "jsonwebtoken";
 
-interface IUser extends Document{
-    _id: string;
-    name: string;
-    email: string;
-};
-
-export interface AuthenticatedRequest extends Request{
-    user?: IUser | null;
+interface IUser extends Document {
+  _id: string;
+  name: string;
+  email: string;
 }
 
-export const isAuth = async(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> =>{
-    try {
-        const authHeader = req.headers.authorization;
+export interface AuthenticatedRequest extends Request {
+  user?: IUser | null;
+}
 
-        if(!authHeader || !authHeader.startsWith("Bearer")){
-            res.status(401).json({
-                message: "Please login - No Auth header",
-            });
-            return; 
-        }
+export const isAuth = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const authHeader = req.headers.authorization;
 
-        const token = authHeader.split(" ")[1];
-
-        const decodedValue= jwt.verify(token as string, process.env.JWT_SECRETE as string) as JwtPayload;
-
-        if(!decodedValue || !decodedValue.user){
-            res.status(401).json({
-                message: "Invalid token",
-            });
-            return;
-        }
-        req.user = decodedValue.user;
-        next();
-
-    } catch (error) {
-        res.status(401).json({
-            message: "Please login - JWT error",
-        });
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+      res.status(401).json({
+        message: "Please login - No Auth header",
+      });
+      return;
     }
+
+    const token = authHeader.split(" ")[1];
+
+    const decodedValue = jwt.verify(
+      token as string,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
+
+    if (!decodedValue || !decodedValue.user) {
+      res.status(401).json({
+        message: "Invalid token",
+      });
+      return;
+    }
+    req.user = decodedValue.user;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "Please login - JWT error",
+    });
+  }
 };
 
-export default isAuth; 
+export default isAuth;
