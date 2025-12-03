0ec430eb22061fa2838d38229a9bf2ad0399cbf3
myuyen0304 <134291922+myuyen0304@users.noreply.github.com>
2025-12-03 23:09:36 +0700

    feat: update code by using socket.io

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import chatRoutes from "./routes/chat.js";
 import cors from "cors";
+import { app, server } from "./config/socket.js";
 
 dotenv.config();
 const port = process.env.PORT;
-const app = express();
+// const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -14,7 +15,7 @@ app.use("/api/v1", chatRoutes);
 
 connectDB()
 
-app.listen(port, () =>{
+server.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
 });
 
