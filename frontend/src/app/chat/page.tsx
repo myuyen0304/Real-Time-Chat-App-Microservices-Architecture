27f0d9eb87df6ba809adee48ed3e04d292b0e59f
myuyen0304 <134291922+myuyen0304@users.noreply.github.com>
2025-12-03 08:12:51 +0700

    feat: implement chat page with real-time messaging

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -2,16 +2,19 @@
 
 import ChatSidebar from "@/components/ChatSidebar";
 import Loading from "@/components/Loading";
-import { useAppData, User } from "@/context/AppContext";
+import { chat_service, useAppData, User } from "@/context/AppContext";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
-
-export interface Message{
+import toast from "react-hot-toast";
+import Cookies from "js-cookie";
+import axios from "axios";
+import ChatHeader from "@/components/ChatHeader";
+export interface Message {
   _id: string;
   chatId: string;
   sender: string;
   text?: string;
-  image?:{
+  image?: {
     url: string;
     publiId: string;
   };
@@ -19,10 +22,18 @@ export interface Message{
   seen: boolean;
   seenAt?: string;
   createdAt: string;
-
 }
 const ChatApp = () => {
-  const { loading, isAuth, logoutUser, chats, user: loggedInUser, users, fetchChats, setChats} = useAppData();
+  const {
+    loading,
+    isAuth,
+    logoutUser,
+    chats,
+    user: loggedInUser,
+    users,
+    fetchChats,
+    setChats,
+  } = useAppData();
 
   const [selectedUser, setSelectedUser] = useState<string | null>(null);
   const [message, setMessage] = useState("");
@@ -31,8 +42,9 @@ const ChatApp = () => {
   const [user, setUser] = useState<User | null>(null);
   const [showAllUser, setShowAllUser] = useState(false);
   const [isTyping, setIsTyping] = useState(false);
-  const [typingTimeOut, setTypingTimeOut] = useState<NodeJS.Timeout | null>(null);
-
+  const [typingTimeOut, setTypingTimeOut] = useState<NodeJS.Timeout | null>(
+    null
+  );
 
   const router = useRouter();
 
@@ -43,25 +55,85 @@ const ChatApp = () => {
   }, [isAuth, router, loading]);
 
   const handleLogout = () => logoutUser();
+
+  async function fetchChat() {
+    const token = Cookies.get("token");
+    try {
+      const { data } = await axios.get(
+        `${chat_service}/api/v1/message/${selectedUser}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setMessages(data.messages);
+      setUser(data.user);
+      await fetchChats();
+    } catch (error) {
+      console.error(error);
+      toast.error("Fail to load messages");
+    }
+  }
+
+  async function createChat(u: User) {
+    try {
+      const token = Cookies.get("token");
+      const { data } = await axios.post(
+        `${chat_service}/api/v1/chat/new`,
+        {
+          userId: loggedInUser?._id,
+          otherUserId: u._id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setSelectedUser(data.chatId);
+      setShowAllUser(false);
+      await fetchChats();
+    } catch (error) {
+      toast.error("Fail to start chat");
+    }
+  }
+
+  useEffect(() => {
+    const loadChat = async () => {
+      if (selectedUser) {
+        await fetchChat();
+      }
+    };
+    loadChat();
+  }, [selectedUser]);
+
   // Không render gì nếu chưa auth
   if (loading || !isAuth) {
     return <Loading />;
   }
 
-  return <div className="min-h-screen flex bg-gray-900 text-white required: overflow-hidden ">
-    <ChatSidebar 
-    sidebarOpen={siderbarOpen} 
-    setSidebarOpen={setSiderbarOpen} 
-    showAllUsers={showAllUser} 
-    setShowAllUsers={setShowAllUser} 
-    users={users} 
-    loggedInUser={loggedInUser} 
-    chats={chats} 
-    selectedUser={selectedUser} 
-    setSelectedUser={setSelectedUser} 
-    handleLogout={handleLogout}/>
-
-    </div>;
+  return (
+    <div className="min-h-screen flex bg-gray-900 text-white overflow-hidden">
+      <ChatHeader setSidebarOpen={setSiderbarOpen} />
+      <ChatSidebar
+        sidebarOpen={siderbarOpen}
+        setSidebarOpen={setSiderbarOpen}
+        showAllUsers={showAllUser}
+        setShowAllUsers={setShowAllUser}
+        users={users}
+        loggedInUser={loggedInUser}
+        chats={chats}
+        selectedUser={selectedUser}
+        setSelectedUser={setSelectedUser}
+        handleLogout={handleLogout}
+        createChat={createChat}
+      />
+      <div className="flex-1 flex flex-col justify-between p-4 backdrop-blur-xl bg-white/5 border border-white/10">
+        {/* Chat content here */}
+      </div>
+    </div>
+  );
 };
 
 export default ChatApp;
